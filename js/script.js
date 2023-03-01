1b6e55ad8e39a06aeb985d8268ff354f54dd62cb
Juan Simon Zubigaray <zubigaray24@gmail.com>
2023-03-01 13:52:37 -0300

    Ahora si final

--- js/script.js
+++ js/script.js
@@ -13,11 +13,16 @@ for(var i = 0; i < btns.length; i++) {
 let body = document.querySelector('body');
 let nombre = document.querySelector('.nombre');
 let btn = document.querySelector('#modo-brillante');
+let textos = document.querySelectorAll('.parr');
 
 function cambiarFondo(e){
   e.preventDefault();
   body.classList.toggle("body-claro");
   nombre.classList.toggle("nombre2");
+
+  for(var i = 0; i < textos.length; i++) {
+    textos[i].classList.toggle("parr-black");
+    };
 }
 
 btn.addEventListener("click", cambiarFondo);
